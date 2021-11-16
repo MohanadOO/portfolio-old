04a7be02f5fa086a44a5b5e42dd1448f433c00f9
MohanadOO <58554988+MohanadOO@users.noreply.github.com>
2021-11-16 19:24:48 +0300

    Add files via upload

---
+++ typing.js
@@ -0,0 +1,34 @@
+const texts = ["a Developer", "an Electronics Engineer", "a Gamer"];
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+
+(function type(){
+  if (count === texts.length){
+  count = 0;
+  }
+  
+  currentText = texts[count]
+  letter = currentText.slice(0, ++index);
+  
+  document.querySelector(".typing").textContent = letter;
+  
+  if (index - 1 === currentText.length){
+    count++;
+    index = 0;
+    // sleep(2000)   
+  }
+  setTimeout(type, 250);
+}())
+
+
+function sleep(milliseconds) {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
+}
+
